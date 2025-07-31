bfa8d9ef242019a4e013f5117b0cfd87241f1fc1
Apoorv <apoorvdeshmukh2945@gmail.com>
2025-07-31 22:24:30 +0530

    Add test.py to .gitignore

--- test.py
+++
@@ -1,4 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyDIG0PeSGZiQR8HH3J61kku86J0juAfE48")
-
-print(genai.GenerativeModel("gemini-1.5-flash").generate_content("Test").text)
